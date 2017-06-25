ab942afc366872ead0e1bc40e55d1d188fdb903d
Allendale Nato <natoallendale@gmail.com>
2017-06-25 12:01:28 +0800

    Add additional later tests

--- app/app.js
+++ app/app.js
@@ -3,3 +3,8 @@ const config = require('./config/config');
 
 const client = new snoowrap(config.reddit);
 
+
+client.getTop('gaming', {limit: 3, time: 'hour'})
+    .then(res => {
+        console.log(res);
+    });
--- app/tests/later.js
+++ app/tests/later.js
@@ -1,12 +1,28 @@
 const later = require('later');
 
 let d = new Date();
-console.log(d);
+console.log(`Current Datetime: ${d}`);
 
 /**
  * Time Periods
  */
 
-console.log(later.second.val(d));
-console.log(later.second.isValid(d, 10));
-console.log(later.second.next(d, 27));
+let phSched = {
+    schedules: [{h: [1, 4, 7, 10, 13, 16, 19, 22]}] // Every 3 Hours (GMT +800)
+};
+let phOccur = later.schedule(phSched).next(5);
+
+let sched = later.parse.text('every 2 mins');
+let occurrences = later.schedule(sched).next(5);
+
+for(let i = 0; i < 5; i++) {
+    console.log(phOccur[i].toString());
+}
+
+for(let i = 0; i < 5; i++) {
+    console.log(occurrences[i].toString());
+}
+
+let e = later.setInterval(() => {
+    console.log(new Date(Date.now()).toString());
+}, sched);
